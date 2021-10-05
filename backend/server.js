108226a130c397e4cf0afc1e68728da5b97f9e54
Technical Priyam <priyamvadofficial1@gmail.com>
2021-10-05 19:56:37 +0530

    implemented custom handlers

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,19 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
 import productRoute from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 connectDB();
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('API is running....');
-})
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
 
 app.use("/api/products", productRoute);
-
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
