eb1ba13cfb391e748487f5c7b2efa3fea0a84162
Technical Priyam <priyamvadofficial1@gmail.com>
2021-10-05 15:16:06 +0530

    preparing sample user data

--- backend/server.js
+++ backend/server.js
@@ -22,4 +22,4 @@ app.get('/api/products', (req, res) => {
   })
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
