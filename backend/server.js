6da7c69df6ee71458946346041d4c8048b0e2d01
Technical Priyam <priyamvadofficial1@gmail.com>
2021-10-07 10:14:53 +0530

    user authentication using email and password in backend

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,21 @@ import connectDB from "./config/db.js";
 import colors from "colors";
 import productRoute from "./routes/productRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config();
 connectDB();
 const app = express();
+app.use(express.json())
 
 app.get("/", (req, res) => {
   res.send("API is running....");
 });
 
 app.use("/api/products", productRoute);
+app.use('/api/users', userRoutes)
+
+// custom error handlers
 app.use(notFound);
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
