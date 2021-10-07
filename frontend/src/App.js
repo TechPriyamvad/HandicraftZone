9544484e75a7241209ea3d3c1007ad5d506e1ba3
Technical Priyam <priyamvadofficial1@gmail.com>
2021-10-07 15:51:15 +0530

    created login screen and implemented login functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,10 @@ import { Container } from "react-bootstrap";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import HomeScreen from "./components/Screens/HomeScreen";
-import { BrowserRouter as Router,Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import ProductScreen from "./components/Screens/ProductScreen";
 import CartScreen from "./components/Screens/CartScreen";
+import LoginScreen from "./components/Screens/LoginScreen";
 
 const App = () => {
   return (
@@ -13,9 +14,10 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <Route path="/" component={HomeScreen} exact/>
-          <Route path="/product/:id" component={ProductScreen}/>
-          <Route path="/cart/:id" component={CartScreen}/>
+          <Route path="/login" component={LoginScreen} />
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id" component={CartScreen} />
         </Container>
       </main>
       <Footer />
