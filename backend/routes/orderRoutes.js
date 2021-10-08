a528db9580ecc58408c4dd205a47509fd4294e7e
Technical Priyam <priyamvadofficial1@gmail.com>
2021-10-08 18:40:47 +0530

    update the payment status of order

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const orderRoute = express.Router()
-import { addOrderItems, getOrderById } from '../controllers/orderController.js'
+import { addOrderItems, getOrderById, updateOrderToPaid } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 orderRoute.route('/').post(protect, addOrderItems)
 orderRoute.route('/:id').get(protect, getOrderById)
+orderRoute.route('/:id/pay').put(protect, updateOrderToPaid)
 
 export default orderRoute
