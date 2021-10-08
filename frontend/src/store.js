8cc4ba3e181949ee0bdf0ca3aa59c4f89c930312
Technical Priyam <priyamvadofficial1@gmail.com>
2021-10-08 23:04:53 +0530

    show logged in user orders in their profile page

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,7 +7,7 @@ import {
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
 import { userDetailsReducer, userLoginReducer, userRegisterReducer, userUpdateProfileReducer } from "./reducers/userReducers";
-import { orderCreateReducer, orderDetailsReducer, orderPayReducer } from "./reducers/orderReducer";
+import { orderCreateReducer, orderDetailsReducer, orderListMyReducer, orderPayReducer } from "./reducers/orderReducer";
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -19,7 +19,8 @@ const reducer = combineReducers({
   userUpdateProfile:userUpdateProfileReducer,
   orderCreate:orderCreateReducer,
   orderDetails:orderDetailsReducer,
-  orderPay:orderPayReducer
+  orderPay:orderPayReducer,
+  orderListMy:orderListMyReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
