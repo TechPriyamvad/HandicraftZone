37fedecca1b3a84e3d53cc718237e81187e43d6a
Technical Priyam <priyamvadofficial1@gmail.com>
2021-10-09 23:15:58 +0530

    preparing for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoute from "./routes/orderRoutes.js";
 import morgan from "morgan";
+import path from 'path'
 
 dotenv.config();
 connectDB();
@@ -16,9 +17,6 @@ if (process.env.NODE_ENV == "development") {
 }
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
 
 app.use("/api/products", productRoute);
 app.use("/api/users", userRoutes);
@@ -28,6 +26,23 @@ app.use("/api/orders", orderRoute);
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+// deployment
+const __dirname = path.resolve()
+// app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 // custom error handlers
 app.use(notFound);
 app.use(errorHandler);
